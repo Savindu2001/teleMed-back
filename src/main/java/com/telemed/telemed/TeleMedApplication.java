924775a581ae10b4289913719be4d653105ef8ee
Savindu Acer <savindu.info@gmail.com>
2023-12-29 20:47:49 +0530

    first commit

---
+++ src/main/java/com/telemed/telemed/TeleMedApplication.java
@@ -0,0 +1,14 @@
+package com.telemed.telemed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeleMedApplication {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/com/telemed/telemed/TeleMedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telemed.telemed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeleMedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
